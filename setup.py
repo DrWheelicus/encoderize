8fb5027b42940d548cb81d6515ee02b052dea4dc
Hayden MacIntyre <haydenpmac@gmail.com>
2025-04-19 23:25:23 -0400

    refactor: rename project to encoderize and update author details
    - Changed project name from "name_visualizer" to "encoderize".
    - Updated author name and email to DrWheelicus and haydenpmac@gmail.com.
    - Modified project URL to reflect the new repository location.

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="name_visualizer",
+    name="encoderize",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
         "svgwrite",
     ],
-    author="Your Name",
-    author_email="your.email@example.com",
+    author="DrWheelicus",
+    author_email="haydenpmac@gmail.com",
     description="A collection of functions to generate various visual representations of text in SVG format",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/name_visualizer",
+    url="https://github.com/DrWheelicus/encoderize",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
